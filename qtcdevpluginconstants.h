2951f690afde147e5db6be0a7352b887ade7fad1
Pascal COMBES <pascom@orange.fr>
2016-05-12 20:28:34 +0200

    Documentation for constants file.

--- qtcdevpluginconstants.h
+++ qtcdevpluginconstants.h
@@ -9,14 +9,30 @@ namespace Constants {
 #define QTC_RUN_CONFIGURATION_ID "QtcDevPlugin.QtcRunConfiguration"
 #define QTC_TEST_RUN_CONFIGURATION_ID "QtcDevPlugin.QtcTestRunConfiguration"
 
-const char QtcRunConfigurationId [] = QTC_RUN_CONFIGURATION_ID;
-const char QtcTestRunConfigurationId [] = QTC_TEST_RUN_CONFIGURATION_ID;
+/*!
+ * \defgroup QtcDevPluginConstants QtcDevPlugin constants
+ * @{
+ */
+const QString PluginName = QLatin1String("QtcDevPlugin");                                           /*!< The name of the plugin (used as root group name in the settings) */
+const QString QtCreatorPluginPriName = QLatin1String("qtcreatorplugin.pri");                        /*!< The name of the project include file for Qt Creator plugins */
 
-const QString QtCreatorPluginPriName = QLatin1String("qtcreatorplugin.pri");
+/*!
+ * \defgroup QtcDevPluginIds QtcDevPlugin Ids
+ * @{
+ */
+const char QtcRunConfigurationId [] = QTC_RUN_CONFIGURATION_ID;                                     /*!< Id for Internal::QtcRunConfiguration */
+const char QtcTestRunConfigurationId [] = QTC_TEST_RUN_CONFIGURATION_ID;                            /*!< Id for Internal::QtcTestRunConfiguration */
+/*!@}*/
 
-const QString WorkingDirectoryId = QLatin1String(QTC_RUN_CONFIGURATION_ID ".WorkingDirectory");
-const QString SettingsPathId = QLatin1String(QTC_RUN_CONFIGURATION_ID ".SettingsPath");
+/*!
+ * \defgroup QtcRunConfigurationSettings QtcRunConfiguration settings
+ * @{
+ */
+const QString WorkingDirectoryKey = QLatin1String(QTC_RUN_CONFIGURATION_ID ".WorkingDirectory");    /*!< Key for working directory path Internal::QtcRunConfiguration */
+const QString SettingsPathKey = QLatin1String(QTC_RUN_CONFIGURATION_ID ".SettingsPath");            /*!< Key for Qt Creator settings path Internal::QtcRunConfiguration */
+/*!@}*/
 
+/*!@}*/
 } // namespace QtcDevPlugin
 } // namespace Constants
 
