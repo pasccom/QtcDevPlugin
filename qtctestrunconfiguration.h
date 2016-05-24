20627fad4242ac186ba89dca57f4ef8131b7a376
Pascal COMBES <pascom@orange.fr>
2016-05-24 20:50:01 +0200

    Added licensing information

--- qtctestrunconfiguration.h
+++ qtctestrunconfiguration.h
@@ -1,3 +1,21 @@
+/* Copyright 2016 Pascal COMBES <pascom@orange.fr>
+ * 
+ * This file is part of QtcDevPlugin.
+ * 
+ * QtcDevPlugin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * QtcDevPlugin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with QtcDevPlugin. If not, see <http://www.gnu.org/licenses/>
+ */
+
 #ifndef QTCTESTRUNCONFIGURATION_H
 #define QTCTESTRUNCONFIGURATION_H
 
