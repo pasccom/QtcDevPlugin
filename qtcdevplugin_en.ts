f125fcbbaead8777153195ee317451e6e273a56b
Pascal COMBES <pascom@orange.fr>
2018-09-09 11:22:17 +0200

    Updated translations.

--- qtcdevplugin_en.ts
+++ qtcdevplugin_en.ts
@@ -4,12 +4,13 @@
 <context>
     <name>QtcDevPlugin::Internal::QtcRunConfiguration</name>
     <message>
-        <location filename="qtcrunconfiguration.cpp" line="62"/>
+        <location filename="qtcrunconfiguration.cpp" line="89"/>
         <source>Run Qt Creator</source>
         <translation>Run Qt Creator</translation>
     </message>
     <message>
-        <location filename="qtcrunconfigurationfactory.cpp" line="87"/>
+        <location filename="qtcrunconfiguration.cpp" line="146"/>
+        <location filename="qtcrunconfigurationfactory.cpp" line="42"/>
         <source>Run Qt Creator with &quot;%1&quot;</source>
         <translation>%1 (Qt Creator)</translation>
     </message>
@@ -17,33 +18,33 @@
 <context>
     <name>QtcDevPlugin::Internal::QtcRunConfigurationWidget</name>
     <message>
-        <location filename="qtcrunconfiguration.cpp" line="130"/>
+        <location filename="qtcrunconfiguration.cpp" line="182"/>
         <source>Working directory:</source>
         <translation>Working directory:</translation>
     </message>
     <message>
-        <location filename="qtcrunconfiguration.cpp" line="132"/>
-        <location filename="qtcrunconfiguration.cpp" line="140"/>
+        <location filename="qtcrunconfiguration.cpp" line="184"/>
+        <location filename="qtcrunconfiguration.cpp" line="192"/>
         <source>Browse...</source>
         <translation>Browse...</translation>
     </message>
     <message>
-        <location filename="qtcrunconfiguration.cpp" line="138"/>
+        <location filename="qtcrunconfiguration.cpp" line="190"/>
         <source>Use alternative settings path</source>
         <translation>Use alternative settings path</translation>
     </message>
     <message>
-        <location filename="qtcrunconfiguration.cpp" line="145"/>
+        <location filename="qtcrunconfiguration.cpp" line="197"/>
         <source>Theme:</source>
         <translation>Theme:</translation>
     </message>
     <message>
-        <location filename="qtcrunconfiguration.cpp" line="213"/>
+        <location filename="qtcrunconfiguration.cpp" line="272"/>
         <source>Choose working directory</source>
         <translation>Choose working directory</translation>
     </message>
     <message>
-        <location filename="qtcrunconfiguration.cpp" line="242"/>
+        <location filename="qtcrunconfiguration.cpp" line="301"/>
         <source>Choose alternative settings path</source>
         <translation>Choose alternative settings path</translation>
     </message>
@@ -51,12 +52,13 @@
 <context>
     <name>QtcDevPlugin::Internal::QtcTestRunConfiguration</name>
     <message>
-        <location filename="qtcrunconfigurationfactory.cpp" line="89"/>
+        <location filename="qtcrunconfigurationfactory.cpp" line="49"/>
+        <location filename="qtctestrunconfiguration.cpp" line="57"/>
         <source>Run Qt Creator tests for &quot;%1&quot;</source>
         <translation>Test %1 (Qt Creator)</translation>
     </message>
     <message>
-        <location filename="qtctestrunconfiguration.cpp" line="25"/>
+        <location filename="qtctestrunconfiguration.cpp" line="43"/>
         <source>Run Qt Creator tests</source>
         <translation>Run Qt Creator tests</translation>
     </message>
@@ -64,37 +66,37 @@
 <context>
     <name>QtcDevPlugin::Widgets::FileTypeValidatingLineEdit</name>
     <message>
-        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="36"/>
+        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="54"/>
         <source>File &quot;%1&quot; does not exist</source>
         <translation>File &quot;%1&quot; does not exist</translation>
     </message>
     <message>
-        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="64"/>
+        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="82"/>
         <source>File does not have one of the required extensions.</source>
         <translation>File does not have one of the required extensions.</translation>
     </message>
     <message>
-        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="74"/>
+        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="92"/>
         <source>Regular files are not accepted</source>
         <translation>Regular files are not accepted</translation>
     </message>
     <message>
-        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="80"/>
+        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="98"/>
         <source>Directories are not accepted</source>
         <translation>Directories are not accepted</translation>
     </message>
     <message>
-        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="93"/>
+        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="111"/>
         <source>File must be readable</source>
         <translation>File must be readable</translation>
     </message>
     <message>
-        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="98"/>
+        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="116"/>
         <source>File must be writable</source>
         <translation>File must be writable</translation>
     </message>
     <message>
-        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="103"/>
+        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="121"/>
         <source>File must be executable</source>
         <translation>File must be executable</translation>
     </message>
--- qtcdevplugin_fr.ts
+++ qtcdevplugin_fr.ts
@@ -1,15 +1,16 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.0" language="fr_FR">
+<TS version="2.1" language="fr_FR">
 <context>
     <name>QtcDevPlugin::Internal::QtcRunConfiguration</name>
     <message>
-        <location filename="qtcrunconfiguration.cpp" line="62"/>
+        <location filename="qtcrunconfiguration.cpp" line="89"/>
         <source>Run Qt Creator</source>
         <translation>Exécuter Qt Creator</translation>
     </message>
     <message>
-        <location filename="qtcrunconfigurationfactory.cpp" line="87"/>
+        <location filename="qtcrunconfiguration.cpp" line="146"/>
+        <location filename="qtcrunconfigurationfactory.cpp" line="42"/>
         <source>Run Qt Creator with &quot;%1&quot;</source>
         <translation>%1 (Qt Creator)</translation>
     </message>
@@ -17,33 +18,33 @@
 <context>
     <name>QtcDevPlugin::Internal::QtcRunConfigurationWidget</name>
     <message>
-        <location filename="qtcrunconfiguration.cpp" line="130"/>
+        <location filename="qtcrunconfiguration.cpp" line="182"/>
         <source>Working directory:</source>
         <translation>Répertoire de travail:</translation>
     </message>
     <message>
-        <location filename="qtcrunconfiguration.cpp" line="132"/>
-        <location filename="qtcrunconfiguration.cpp" line="140"/>
+        <location filename="qtcrunconfiguration.cpp" line="184"/>
+        <location filename="qtcrunconfiguration.cpp" line="192"/>
         <source>Browse...</source>
         <translation>Parcourir...</translation>
     </message>
     <message>
-        <location filename="qtcrunconfiguration.cpp" line="138"/>
+        <location filename="qtcrunconfiguration.cpp" line="190"/>
         <source>Use alternative settings path</source>
         <translation>Utiliser une configuration alternative</translation>
     </message>
     <message>
-        <location filename="qtcrunconfiguration.cpp" line="145"/>
+        <location filename="qtcrunconfiguration.cpp" line="197"/>
         <source>Theme:</source>
         <translation>Thème:</translation>
     </message>
     <message>
-        <location filename="qtcrunconfiguration.cpp" line="213"/>
+        <location filename="qtcrunconfiguration.cpp" line="272"/>
         <source>Choose working directory</source>
         <translation>Choisissez le répertoire de travail</translation>
     </message>
     <message>
-        <location filename="qtcrunconfiguration.cpp" line="242"/>
+        <location filename="qtcrunconfiguration.cpp" line="301"/>
         <source>Choose alternative settings path</source>
         <translation>Choisissez le chemin de la configuration alternative</translation>
     </message>
@@ -51,12 +52,13 @@
 <context>
     <name>QtcDevPlugin::Internal::QtcTestRunConfiguration</name>
     <message>
-        <location filename="qtcrunconfigurationfactory.cpp" line="89"/>
+        <location filename="qtcrunconfigurationfactory.cpp" line="49"/>
+        <location filename="qtctestrunconfiguration.cpp" line="57"/>
         <source>Run Qt Creator tests for &quot;%1&quot;</source>
         <translation>Tester %1 (Qt Creator)</translation>
     </message>
     <message>
-        <location filename="qtctestrunconfiguration.cpp" line="25"/>
+        <location filename="qtctestrunconfiguration.cpp" line="43"/>
         <source>Run Qt Creator tests</source>
         <translation>Tester dans Qt Creator</translation>
     </message>
@@ -64,37 +66,37 @@
 <context>
     <name>QtcDevPlugin::Widgets::FileTypeValidatingLineEdit</name>
     <message>
-        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="36"/>
+        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="54"/>
         <source>File &quot;%1&quot; does not exist</source>
         <translation>Le fichier &quot;%1&quot; n&apos;existe pas</translation>
     </message>
     <message>
-        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="64"/>
+        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="82"/>
         <source>File does not have one of the required extensions.</source>
         <translation>Le ficher n&apos;a pas l&apos;une des extensions requises.</translation>
     </message>
     <message>
-        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="74"/>
+        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="92"/>
         <source>Regular files are not accepted</source>
         <translation>Les fichiers reguliers ne sont pas acceptés</translation>
     </message>
     <message>
-        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="80"/>
+        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="98"/>
         <source>Directories are not accepted</source>
         <translation>Les répertoires ne sont pas acceptés</translation>
     </message>
     <message>
-        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="93"/>
+        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="111"/>
         <source>File must be readable</source>
         <translation>Le fichier doit être lisible</translation>
     </message>
     <message>
-        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="98"/>
+        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="116"/>
         <source>File must be writable</source>
         <translation>Le fichier doit être modifiable</translation>
     </message>
     <message>
-        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="103"/>
+        <location filename="Widgets/filetypevalidatinglineedit.cpp" line="121"/>
         <source>File must be executable</source>
         <translation>Le fichier doit être exécutable</translation>
     </message>
